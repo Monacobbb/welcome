7ba9417d4f9396f3fe908d02199fc39a75ca5f7b
Monacobbb <113931835+Monacobbb@users.noreply.github.com>
2022-09-20 02:23:10 +0900

    version3

---
+++ Pracetice_2.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Practice_1
+{
+    class Pracetice_2
+    {
+        static void Main_1(string[] args)
+        {
+            Console.WriteLine("version3");
+        }
+    }
+}
---
+++ practice 1.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Practice_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("여러분, 안녕하세요?");
+        }
+    }
+}
